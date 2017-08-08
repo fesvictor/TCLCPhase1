f0e30b301dad3dc42e9b36612e089cf2e4c48645
newlyzher <newlyzher@gmail.com>
2017-08-09 01:02:26 +0800

    update

--- AnalysisLib/ProcessFile.py
+++ AnalysisLib/ProcessFile.py
@@ -27,23 +27,19 @@ def getObjectList(ObjectType, FileName): #get data from record file(replaced)
     return object_list
 
 def UpdateRecord(_location, _table):
-    year_list = ['00','01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17']
+    year_list = ['17']
     month_list = ['00', '01', '02','03','04','05','06','07','08','09','10','11','12']
-    day_list = ['00','01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31']
     for _year in year_list:
         if _year in _table:
             for _month in month_list:
                 if _month in _table[_year]:
-                    if _table[_year][_month] != {}:
-                        with open(_location + '/' + _year + '_' + _month + '.csv', 'w') as outFile:
+                    #if _table[_year][_month] != {}:
+                        with open(_location + '/' + '20' + _year + '_' + _month + '.csv', 'w') as outFile:
                             outFile.write("name,scale,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31")
                             for key, value in _table[_year][_month].items():
                                 for i, x in enumerate(value['01'], 0):
                                     outFile.write('\n')
-                                    outFile.write(key + ',' + str(i+1) + ',' + str(value['01'][i]) + ',' + str(value['02'][i]) + ','+ str(value['03'][i]) + ','+ str(value['04'][i]) + ','+ str(value['05'][i]) + ','+ str(value['06'][i]) + ','+ str(value['07'][i]) + ','+ str(value['08'][i]) + ','+ str(value['09'][i]) + ','+ str(value['10'][i]) + ','+ str(value['11'][i]) + ','+ str(value['12'][i]) + ','+ str(value['13'][i]) + ','+ str(value['14'][i]) + ','+ str(value['15'][i]) + ','+ str(value['16'][i]) + ','+ str(value['17'][i]) + ','+ str(value['18'][i]) + ','+ str(value['19'][i]) + ','+ str(value['20'][i]) + ','+ str(value['21'][i]) + ','+ str(value['22'][i]) + ','+ str(value['23'][i]) + ','+ str(value['24'][i]) + ','+ str(value['25'][i]) + ','+ str(value['26'][i]) + ','+ str(value['27'][i]) + ','+ str(value['28'][i]) + ','+ str(value['29'][i]) + ','+ str(value['30'][i]) + ','+ str(value['31'][i]))
-                                    
-        else:
-            pass
+                                    outFile.write(key + ',' + str(i+1) + ',' + str(value['01'][i]) + ',' + str(value['02'][i]) + ','+ str(value['03'][i]) + ','+ str(value['04'][i]) + ','+ str(value['05'][i]) + ','+ str(value['06'][i]) + ','+ str(value['07'][i]) + ','+ str(value['08'][i]) + ','+ str(value['09'][i]) + ','+ str(value['10'][i]) + ','+ str(value['11'][i]) + ','+ str(value['12'][i]) + ','+ str(value['13'][i]) + ','+ str(value['14'][i]) + ','+ str(value['15'][i]) + ','+ str(value['16'][i]) + ','+ str(value['17'][i]) + ','+ str(value['18'][i]) + ','+ str(value['19'][i]) + ','+ str(value['20'][i]) + ','+ str(value['21'][i]) + ','+ str(value['22'][i]) + ','+ str(value['23'][i]) + ','+ str(value['24'][i]) + ','+ str(value['25'][i]) + ','+ str(value['26'][i]) + ','+ str(value['27'][i]) + ','+ str(value['28'][i]) + ','+ str(value['29'][i]) + ','+ str(value['30'][i]) + ','+ str(value['31'][i]))                                    
 
 def xUpdateRecord(FileName, object_list): #update the record file(replaced)
     from time import strftime
--- week8analysis.py
+++ week8analysis.py
@@ -4,13 +4,13 @@ from ReadParameterFile import get_parameter_dict
 from AnalysisLib import Year
 
 def getResult():
-    yearTable = {}
+    yearTable = Year.definexYear(['17'], party_list, leader_list)
     word_list = []
-    word_list += ProcessJsonData(param["json.files"])
-    word_list += ProcessFbData(param["facebook.files"])
-    word_list += ProcessMalaysiaKiniData(param["malaysiakini.files"])
+    #word_list += ProcessJsonData(param["json.files"])
+    #word_list += ProcessFbData(param["facebook.files"])
+    #word_list += ProcessMalaysiaKiniData(param["malaysiakini.files"])
     word_list += ProcessLowyatData(param["lowyat.files"])
-    word_list += ProcessTweetData(param["twitter.files"])
+    #word_list += ProcessTweetData(param["twitter.files"])
     
     for word in word_list:  #process every sentence
         word[0] = word[0].lower()
@@ -19,31 +19,23 @@ def getResult():
                 #attitude likert scale
                 if search_scale("Attitude",1,word[0]): # search whether this word in database
                     yearTable = Year.addScale(yearTable, word[3], word[2], word[1], party.getName(), 0, "party")
-                elif search_scale("Attitude",2,word[0]):
-                    yearTable = Year.addScale(yearTable, word[3], word[2], word[1], party.getName(), 1, "party")
                 elif search_scale("Attitude",3,word[0]):
-                    yearTable = Year.addScale(yearTable, word[3], word[2], word[1], party.getName(), 2, "party")
-                
-        for govtPolicy in govtPolicy_list: 
-            if govtPolicy.getName() in word[0]: 
-                #perception likert scale
-                if search_scale("Perception",1,word[0]): 
-                    yearTable = Year.addScale(yearTable, word[3], word[2], word[1], govtPolicy.getName(), 0, "policy")
-                elif search_scale("Perception",2,word[0]):
-                    yearTable = Year.addScale(yearTable, word[3], word[2], word[1], govtPolicy.getName(), 1, "policy")
-                elif search_scale("Perception",3,word[0]):
-                    yearTable = Year.addScale(yearTable, word[3], word[2], word[1], govtPolicy.getName(), 2, "policy")
-                elif search_scale("Perception",4,word[0]):
-                    yearTable = Year.addScale(yearTable, word[3], word[2], word[1], govtPolicy.getName(), 3, "policy")
-                elif search_scale("Perception",5,word[0]):
-                    yearTable = Year.addScale(yearTable, word[3], word[2], word[1], govtPolicy.getName(), 4, "policy")
-                
+                    yearTable = Year.addScale(yearTable, word[3], word[2], word[1], party.getName(), 1, "party")
+           
+        #for govtPolicy in govtPolicy_list: 
+        #    if govtPolicy.getName() in word[0]: 
+        #        #perception likert scale
+        #        if search_scale("Perception",1,word[0]): 
+        #            yearTable = Year.addScale(yearTable, word[3], word[2], word[1], govtPolicy.getName(), 0, "policy")
+        #        elif search_scale("Perception",2,word[0]):
+        #            yearTable = Year.addScale(yearTable, word[3], word[2], word[1], govtPolicy.getName(), 1, "policy")
+        
         for leader in leader_list: 
             if leader.getName() in word[0]: 
                 #popularity likert scale
                 if search_scale("Popularity", 1, word[0]):
                     yearTable = Year.addScale(yearTable, word[3], word[2], word[1], leader.getName(), 0, "leader")
-                elif search_scale("Popularity", 2, word[0]):
+                elif search_scale("Popularity", 5, word[0]):
                     yearTable = Year.addScale(yearTable, word[3], word[2], word[1], leader.getName(), 1, "leader")
     return yearTable
 
