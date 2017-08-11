77a17a69b400e448fee5be8f6ed354c18db8fbc9
gary20m <very_gary@hotmail.com>
2017-08-12 03:32:37 +0800

    Perception plotter v2
    12/08/2017

--- perception plotter2.py
+++ perception plotter2.py
@@ -3,6 +3,14 @@ import pandas as pd
 import re
 import datetime
 
+def plot_subplots(df, axes):
+    df1 = df.pivot(index='name', columns='scale', values=str(df.columns[2]))
+    splt1 = df1.plot(ax=axes, kind='bar')
+    splt1.set_xlabel("")
+    splt1.set_xticklabels(splt1.get_xticklabels(), fontsize=10, rotation=90)
+    splt1.legend().remove()
+    splt1.set_title("Day "+str(df.columns[2]))
+
 def get_top3_daily(df_main):
     top3_daily_list = []
     for i in range(1,32):
@@ -24,74 +32,49 @@ def date_converter(filename):
     return datetime.datetime.strftime(date, '%B %Y')
     
 def plot_perception():
-    
-    my_dpi = 70#96
+    plt.ioff()
+    my_dpi = 150#96
     filename = 'temp/2017_01.csv'
     date_str = date_converter(filename)
     df_main = pd.read_csv(filename)
     top3_daily_list = get_top3_daily(df_main)
     
-#    print(top3_daily_list[0].pivot(index='name', columns='scale'))
+
     
-    fig, axes = plt.subplots(nrows=4, ncols=10, figsize=(800/my_dpi, 600/my_dpi), dpi=my_dpi, sharey=True, sharex=False)
-    fig.autofmt_xdate(rotation=90, ha='right')
-    fig.suptitle("Perceptions (Jan)", y=0.92, fontsize=18)
+#   print(top3_daily_list[0].pivot(index='name', columns='scale'))
     
+    fig, axes = plt.subplots(nrows=4, ncols=10, figsize=(3840/my_dpi, 2160/my_dpi), dpi=my_dpi, sharey=True, sharex=False)
+    fig.suptitle("Perceptions (Top 3 Daily) - " + date_str, y=0.92, fontsize=25)
+    
+    #Delete extra subplot axes
+    for i in range(1,10):
+        fig.delaxes(axes[3,i])
+        
+    for df_, ax_ in zip(top3_daily_list, axes.reshape(-1)):
+        plot_subplots(df_, ax_)
+    
+    #Create invisible surrounding subplot for labeling purposes
     fig.add_subplot(111, frameon=False)
     plt.tick_params(labelcolor='none', top='off', bottom='off', left='off', right='off')
-    plt.xlabel("Name", labelpad=90)
-    plt.ylabel("Count")
-    plt.subplots_adjust(hspace=0.15, wspace=0.1)
-    plt.legend()
+    plt.xlabel("Name", fontsize=20)
+    plt.ylabel("Count", fontsize=20)
+    plt.subplots_adjust(hspace=1, wspace=0.1)
     
-    df1 = top3_daily_list[0].pivot(index='name', columns='scale', values='1')
-    splt1 = df1.plot(ax=axes[0,0], kind='bar')
-#    splt1.set_ylabel("")
-#    splt1.set_xticklabels(['a','b','c'])
-    #splt1.set_yticks([0,5,10,15,20,25,30,35,40])
-    splt1.legend().remove()
-    splt1.set_title("Day "+str(1))
-#    
-#    
-#    for i in range(1,11):
-#        df1 = df.pivot(index='name', columns='scale', values=str(i))
-#        splt1 = df1.plot(ax=axes[0,i-1], kind='bar')
-#        splt1.set_ylabel("")
-#        splt1.set_xlabel("")    
-#        splt1.set_yticks([0,5,10,15,20,25,30,35,40])
-#        splt1.legend().remove()
-#        splt1.set_title("Day "+str(i))
-#        
-#    for i in range(1,11):
-#        df1 = df.pivot(index='name', columns='scale', values=str(i+10))
-#        splt1 = df1.plot(ax=axes[1,i-1], kind='bar')
-#        splt1.set_ylabel("")
-#        splt1.set_xlabel("")
-#        #splt1.set_yticks([1,2,3,4,5])
-#        splt1.legend().remove()
-#        splt1.set_title("Day "+str(i+10))
-#        
-#    for i in range(1,11):
-#        df1 = df.pivot(index='name', columns='scale', values=str(i+20))
-#        splt1 = df1.plot(ax=axes[2,i-1], kind='bar')
-#        splt1.set_ylabel("")
-#        splt1.set_xlabel("")
-#        #splt1.set_yticks([1,2,3,4,5])
-#        splt1.legend().remove()
-#        splt1.set_title("Day "+str(i+20))
-#        
-#    for i in range(1,2):
-#        df1 = df.pivot(index='name', columns='scale', values=str(i+30))
-#        splt1 = df1.plot(ax=axes[3,i-1], kind='bar')
-#        splt1.set_ylabel("")
-#        splt1.set_xlabel("")
-#        #splt1.set_yticks([1,2,3,4,5])
-#        splt1.legend().remove()
-#        splt1.set_title("Day "+str(i+30))
-#    
-    splt1.legend(bbox_to_anchor=(11.05, 4), loc='lower left', borderaxespad=0.)
+#    df = top3_daily_list[0]
+#    df1 = top3_daily_list[0].pivot(index='name', columns='scale', values='1')
+#    splt1 = df1.plot(ax=axes[0,0], kind='bar')
+#    splt1.set_xlabel("")
+#    splt1.set_xticklabels(splt1.get_xticklabels(), fontsize=10, rotation=45)
+#    splt1.legend().remove()
+#    splt1.set_title("Day "+str(df.columns[2]))
     
-    plt.savefig('temp.png', bbox_inches='tight')
+    #Custom legend
+    lines, labels = axes[0,0].get_legend_handles_labels()
+    fig.legend(lines, ['Positive','Negative'], bbox_to_anchor=(0.95, 0.885), bbox_transform=plt.gcf().transFigure)
+    #Save plot
+    plt.savefig('temp.png', bbox_inches='tight', dpi=300)
+    plt.close(fig) #Disable console print
+
         
 #Main
 plot_perception()
