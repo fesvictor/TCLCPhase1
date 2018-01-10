67d4bb3462e7592e446c2d796d05be22fc30a580
WJH <hou32hou@gmail.com>
2018-01-10 18:37:45 +0800

    Corrected imports in get_object_list.py

--- AnalysisLib/process_file/get_object_list.py
+++ AnalysisLib/process_file/get_object_list.py
@@ -1,22 +1,16 @@
-# The 2 lines below allow you to import modules from parent directory
-# For more info, refer https://stackoverflow.com/questions/16780014/import-file-from-parent-directory/16780068
-import os
-import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from TCLCPhase1.get_parameter_dict import get_parameter_dict
 def get_object_list(object_type): #get data from record file(replaced)
     if object_type == "party":
-        from Party import instantObject
+        from TCLCPhase1.AnalysisLib.Party import Party as object
     elif object_type == "govtPolicy":
-        from GovtPolicy import instantObject
+        from TCLCPhase1.AnalysisLib.GovtPolicy import GovtPolicy as object
     elif object_type == "leader":
-        from Leader import instantObject
+        from TCLCPhase1.AnalysisLib.Leader import Leader as object
     else:
          raise ValueError(object_type + "is not a recognized object_type")
     file_name = get_parameter_dict()["target"] + "/" + object_type + ".txt"
     object_list = []
     with open(file_name) as record_file:
         for row in record_file.read().splitlines():
-            object_list.append(instantObject(row))
+            object_list.append(object(row))
     return object_list
