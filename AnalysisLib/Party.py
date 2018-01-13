4c0b58674daa9feb8aed3581b707de167ca88c3d
WJH <hou32hou@gmail.com>
2018-01-13 10:06:58 +0800

    Add unit test for Party.py

--- AnalysisLib/Party.py
+++ AnalysisLib/Party.py
@@ -1,27 +1,20 @@
 class Party:
     def __init__(self, _name):
         self.name = _name
-        self.scale1 = 0
-        self.scale2 = 0
-        self.scale3 = 0
-        
-    def getName(self):
+        self.negative = 0
+        self.positive = 0
+
+    def get_name(self):
         return self.name
 
-    def getScale(self):
-        return [self.scale1, self.scale2, self.scale3]
-    
-    def increScale1(self):
-        self.scale1 += 1
-    
-    def increScale2(self):
-        self.scale2 += 1
-    
-    def increScale3(self):
-        self.scale3 += 1
+    def get_scale(self):
+        return [self.negative, self.positive]
+
+    def incre_negative(self):
+        self.negative += 1
+
+    def incre_positive(self):
+        self.positive += 1
 
     def __str__(self):
-        return f"\nscale1 = {self.scale1}\nscale2 = {self.scale2}\nscale3 = {self.scale3}\n" 
-
-def instantObject(Name):
-    return Party(Name)
+        return f"\nname={self.name}\nnegative={self.negative}\npositive={self.positive}\n"
--- AnalysisLib/tests/test_Party.py
+++ AnalysisLib/tests/test_Party.py
@@ -1,3 +1,26 @@
 from TCLCPhase1.AnalysisLib.Party import Party
-def test1():
-    print(Party("Rocket"))
+
+
+def test_constructor():
+    x = Party("Barisan National")
+    assert x.name == "Barisan National"
+    assert x.negative == 0
+    assert x.positive == 0
+
+def test_get_name():
+    x = Party("Rocket")
+    assert x.get_name() == "Rocket"
+
+def test_incre_negative():
+    x = Party("MCA")
+    x.incre_negative()
+    assert x.negative == 1
+
+def test_incre_positive():
+    x = Party("MCA")
+    x.incre_positive()
+    assert x.positive == 1
+
+def test_str():
+    x = Party("UTAR")
+    assert x.__str__() == f"\nname=UTAR\nnegative=0\npositive=0\n"
