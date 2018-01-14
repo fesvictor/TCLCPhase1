140f77103d6b094f8b5e1809d4935c23d24303b6
WJH <hou32hou@gmail.com>
2018-01-15 07:22:07 +0800

    Refactored define_x_year and wrote test for it

--- AnalysisLib/analysis/define_x_year.py
+++ AnalysisLib/analysis/define_x_year.py
@@ -1,15 +1,25 @@
 def define_x_year(year, party_list, leader_list):
-    #leader_dict = {'01':[0,0],'02':[0,0],'03':[0,0],'04':[0,0],'05':[0,0],'06':[0,0],'07':[0,0],'08':[0,0],'09':[0,0],'10':[0,0],'11':[0,0],'12':[0,0],'13':[0,0],'14':[0,0],'15':[0,0],'16':[0,0],'17':[0,0],'18':[0,0],'19':[0,0],'20':[0,0],'21':[0,0],'22':[0,0],'23':[0,0],'24':[0,0],'25':[0,0],'26':[0,0],'27':[0,0],'28':[0,0],'29':[0,0],'30':[0,0],'31':[0,0]}
-    xyear = {}
-    for oyear in year:
-        xyear[oyear] = {'01':{},'02':{},'03':{},'04':{},'05':{},'06':{},'07':{},'08':{} , '09':{}}
-        
-        for key, value in xyear[oyear].items():
-            xyear[oyear][key]['party'] = {}
+    year_table = {}
+    for year in year:
+        year_table[year] = get_month_list();
+        for key, value in year_table[year].items():
+            year_table[year][key]['party'] = {}
             for party in party_list:
-                    xyear[oyear][key]['party'][party.get_name()] = {'01':[0,0],'02':[0,0],'03':[0,0],'04':[0,0],'05':[0,0],'06':[0,0],'07':[0,0],'08':[0,0],'09':[0,0],'10':[0,0],'11':[0,0],'12':[0,0],'13':[0,0],'14':[0,0],'15':[0,0],'16':[0,0],'17':[0,0],'18':[0,0],'19':[0,0],'20':[0,0],'21':[0,0],'22':[0,0],'23':[0,0],'24':[0,0],'25':[0,0],'26':[0,0],'27':[0,0],'28':[0,0],'29':[0,0],'30':[0,0],'31':[0,0]}
-            xyear[oyear][key]['leader'] = {}
+                year_table[year][key]['party'][party.get_name()] = get_day_list()
+            year_table[year][key]['leader'] = {}
             for leader in leader_list:
-                    xyear[oyear][key]['leader'][leader.get_name()] = {'01':[0,0],'02':[0,0],'03':[0,0],'04':[0,0],'05':[0,0],'06':[0,0],'07':[0,0],'08':[0,0],'09':[0,0],'10':[0,0],'11':[0,0],'12':[0,0],'13':[0,0],'14':[0,0],'15':[0,0],'16':[0,0],'17':[0,0],'18':[0,0],'19':[0,0],'20':[0,0],'21':[0,0],'22':[0,0],'23':[0,0],'24':[0,0],'25':[0,0],'26':[0,0],'27':[0,0],'28':[0,0],'29':[0,0],'30':[0,0],'31':[0,0]}
-    #print(xyear)
-    return xyear
+                year_table[year][key]['leader'][leader.get_name()] = get_day_list()
+    return year_table
+
+
+def get_month_list():
+    # Note: range(1,3) = [1,2]
+    month_list = dict.fromkeys(map(pad2digit, range(1, 13)), {})
+    return month_list
+
+def get_day_list():
+    day_list = dict.fromkeys(map(pad2digit, range(1, 32)), [0, 0])
+    return day_list
+
+def pad2digit(x):
+    return str(x).zfill(2)
