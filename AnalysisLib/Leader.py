4d55996bb97569bb04f69b778ac2a829ba5b4155
WJH <hou32hou@gmail.com>
2018-01-14 10:40:38 +0800

    Refactored Party and Leader to be inherited from the same class named BaseObject - since they have the same method

--- AnalysisLib/Leader.py
+++ AnalysisLib/Leader.py
@@ -1,20 +1,3 @@
-class Leader:
-    def __init__(self, _name):
-        self.name = _name
-        self.scale1 = 0
-        self.scale2 = 0
-
-    def getName(self):
-        return self.name
-    
-    def getScale(self):
-        return [self.scale1, self.scale2]
-
-    def increScale1(self):
-        self.scale1 += 1
-    
-    def increScale2(self):
-        self.scale2 += 1
-
-def instantObject(Name):
-    return Leader(Name)
+from TCLCPhase1.AnalysisLib.BaseObject import BaseObject
+class Leader(BaseObject):
+    pass
--- AnalysisLib/tests/test_BaseObject.py
+++ AnalysisLib/tests/test_BaseObject.py
@@ -1,26 +1,25 @@
-from TCLCPhase1.AnalysisLib.Party import Party
-
+from TCLCPhase1.AnalysisLib.BaseObject import BaseObject
 
 def test_constructor():
-    x = Party("Barisan National")
+    x = BaseObject("Barisan National")
     assert x.name == "Barisan National"
     assert x.negative == 0
     assert x.positive == 0
 
 def test_get_name():
-    x = Party("Rocket")
+    x = BaseObject("Rocket")
     assert x.get_name() == "Rocket"
 
 def test_incre_negative():
-    x = Party("MCA")
+    x = BaseObject("MCA")
     x.incre_negative()
     assert x.negative == 1
 
 def test_incre_positive():
-    x = Party("MCA")
+    x = BaseObject("MCA")
     x.incre_positive()
     assert x.positive == 1
 
 def test_str():
-    x = Party("UTAR")
+    x = BaseObject("UTAR")
     assert x.__str__() == f"\nname=UTAR\nnegative=0\npositive=0\n"
