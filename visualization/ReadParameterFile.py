8313ab8fd64e44db8f8a12c2dbe10376c5fedc03
Chong Shao Hong <cshong.gcc@hotmail.com>
2018-01-16 21:40:45 +0800

    Added params, methods list_all_files method to grep all files from the root dir recursively downwards. Trim off whitespaces from splitting string to list

--- visualization/ReadParameterFile.py
+++ visualization/ReadParameterFile.py
@@ -4,9 +4,9 @@ def get_parameter_dict(filename):
         for row in InFile:
             if row[0] is not "#" and row[0] is not "\n" and row[0] is not " ":
                 row = row.replace("\n","").split("=")
-
+                
                 if row[1].find(",") >= 0:
-                    row[1] = [s for s in row[1].split(",")]
+                    row[1] = [s.strip() for s in row[1].split(",")]
 
                 parameter_dict[row[0]] = row[1]
 
