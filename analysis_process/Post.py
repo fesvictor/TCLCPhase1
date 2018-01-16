50e45f6fc18f28940d8bcb594ffe71a38d88d077
WJH <hou32hou@gmail.com>
2018-01-16 22:55:41 +0800

    Added analysis_process folder

---
+++ analysis_process/Post.py
@@ -0,0 +1,9 @@
+class Post:
+    def __init__(self):
+        self.value  = ""
+        self.date   = ""
+        self.source = ""
+        self.related_to  = []
+        self.semantic_value = []
+        
+
