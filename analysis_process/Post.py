bf780f6cddc8e5430fd46fe8c0739c9ea6bcc8d8
WJH <hou32hou@gmail.com>
2018-01-17 05:26:33 +0800

    Add documentation for Post

--- analysis_process/Post.py
+++ analysis_process/Post.py
@@ -1,9 +1,20 @@
+'''
+An example post would be like the following : 
+Post = {
+    value = 'I support BN!',
+    date = '31-12-2017',
+    source = 'Facebook',
+    related_to = 'BN',
+    semantic_value = 1 # 1 = positive, 0 =neutral, -1 = negative
+}
+'''
 class Post:
     def __init__(self):
-        self.value  = ""
         self.date   = ""
+        self.value  = ""
         self.source = ""
         self.related_to  = []
         self.semantic_value = []
         
-
+    def __str__(self):
+        return "date: %s\nvalue: %s\nsource: %s\nrelated_to: %s\nsemantic_value: %s" % (self.date, self.value, self.source, self.related_to, self.semantic_value)
