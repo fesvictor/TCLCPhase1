86a7e287f54f4372246822170fa83f9c14bb70c2
WJH <hou32hou@gmail.com>
2018-01-19 09:26:48 +0800

    Added script for running the first process

--- analysis_process/_1_process_raw_data/README.md
+++ analysis_process/_1_process_raw_data/README.md
@@ -3,7 +3,6 @@ Make sure you are in the directory of
 `\_1\_process\_raw\_data`  
 Then, run the following command:
 ```
-cd ..
-py -m analysis_process._1_process_raw_data.main
+run
 ```
 The output will be store in the dir of `output`.
--- analysis_process/_1_process_raw_data/store_output.py
+++ analysis_process/_1_process_raw_data/store_output.py
@@ -4,7 +4,7 @@ def store_output(list_of_posts, source):
     parent_dir = 'analysis_process/_1_process_raw_data/'
     file_dir = parent_dir + 'output/' + source + '_output.json'
     with open(file_dir, 'w+') as file:
-        json.dump([ob.__dict__ for ob in list_of_posts], file)
+        json.dump([ob.__dict__ for ob in list_of_posts], file, indent=2)
     # The following code is how you decode the json
     # with open(file_dir, 'r') as file:
     #     data = json.load(file)
