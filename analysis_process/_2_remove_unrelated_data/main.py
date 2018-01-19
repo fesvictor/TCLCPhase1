2ecf84c3b2ddd0bdd419e65a951ddda2db7ff89a
WJH <hou32hou@gmail.com>
2018-01-19 11:54:54 +0800

    Added main.py for second step of analysis_process

---
+++ analysis_process/_2_remove_unrelated_data/main.py
@@ -0,0 +1,33 @@
+from analysis_process._2_remove_unrelated_data.load_labels import load_labels
+from analysis_process.load_posts import load_posts
+from analysis_process.save_posts import save_posts
+from os import listdir
+
+def main():
+    all_posts = get_posts()
+    all_labels = get_labels()
+    for post in all_posts:
+        for label in all_labels:
+            if label in post['value']:
+                post['related_to'] = label
+    purified = [x for x in all_posts if x['related_to'] is not None]
+
+    save_posts(purified, 'analysis_process/_2_remove_unrelated_data/output.json')
+
+
+
+def get_labels():
+    labels_dir = 'data/target/'
+    leaders = load_labels(labels_dir + 'leader.txt')
+    parties = load_labels(labels_dir + 'party.txt')
+    return leaders + parties
+
+
+def get_posts():
+    posts_dir = 'analysis_process/_1_process_raw_data/output'
+    result = []
+    for file_name in listdir(posts_dir):
+        result += load_posts(posts_dir + '/' + file_name)
+    return result
+
+main()
--- analysis_process/save_posts.py
+++ analysis_process/save_posts.py
@@ -2,5 +2,7 @@ import json
 
 def save_posts(list_of_posts, file_name):
     with open(file_name, 'w+') as file:
-        json.dump([ob.__dict__ for ob in list_of_posts], file, indent=2)
-    
+        if hasattr(list_of_posts[0], '__dict__'):
+            json.dump([ob.__dict__ for ob in list_of_posts], file, indent=2)
+        else:
+            json.dump([ob for ob in list_of_posts], file, indent=2)
