aa0a7c6ae13a916c4edc01889f6e7c95d7bd83b5
newlyzher <newlyzher@gmail.com>
2018-01-21 17:57:40 +0800

    update

--- AnalysisLib/process_file/process_tweet_data.py
+++ AnalysisLib/process_file/process_tweet_data.py
@@ -6,17 +6,21 @@ def process_tweet_data(FilePath):
     lookup_table = {'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06', 'Jul': '07', 'Aug': '08','Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12'} 
     for FileName in listdir(FilePath): 
             with open(FilePath + "/" + FileName, encoding = 'utf-8') as InFile: 
-                k = InFile.read().split('=======')
-                k[0] = k[0].replace('<<<<<<< HEAD','')
-                k[1] = k[1].replace('>>>>>>> analysis','')
                 print(FileName)
+                k = InFile.read()
+                if '\n=======\n' in k:
+                    k = k.split('\n=======\n')
+                    k[0] = k[0].replace('<<<<<<< HEAD','')
+                    k[1] = k[1].replace('>>>>>>> analysis','')
+                else:
+                    k = [k]                    
                 for j in k:
                     df = read_csv(StringIO(j))                
                     for index, row in df.iterrows(): 
                         try:
                             _date = df.loc[index]['created_at'].split(" ") 
                             word_list.append([df.loc[index]['text'], _date[2],lookup_table[_date[1]],_date[5][2:]]) 
-                            print('tweet:', df.loc[index]['text'], _date)
+                            #print('tweet:', df.loc[index]['text'], _date)
                         except KeyError: 
                             pass              
     #print(word_list) 
