16c1406e09910abb3ce4a4b00f339a9853612950
WJH <hou32hou@gmail.com>
2018-01-21 10:42:07 +0800

    Updated step 1 to output posts into seperate files instead of one files - so that if error occur some files still get to be parsed

--- analysis_process/_1_process_raw_data/main.py
+++ analysis_process/_1_process_raw_data/main.py
@@ -6,15 +6,18 @@ from analysis_process._1_process_raw_data.parse_blog import parse_blog
 PARENT_DIR = 'data/scraperesults/'
 FACEBOOK_DIR = PARENT_DIR + 'facebook/'
 BLOG_DIR = PARENT_DIR + 'blog/'
-ALL_POSTS = []
 
 
-for file in os.listdir(FACEBOOK_DIR):
-    ALL_POSTS += parse_facebook(FACEBOOK_DIR + file)
-
-for file in os.listdir(BLOG_DIR):
-    ALL_POSTS += parse_blog(BLOG_DIR + file)
-
-save_posts(ALL_POSTS, 'analysis_process/_1_process_raw_data/output/all_output.json')
+def main():
+    parse_files_from(FACEBOOK_DIR, parse_facebook)
+    parse_files_from(BLOG_DIR, parse_blog)
 
 
+def parse_files_from(directory, parser):
+    posts = []
+    for file in os.listdir(directory):
+        posts += parser(directory + file)
+        save_posts(posts, f'analysis_process/_1_process_raw_data/output/{file.replace(".csv", "")}.json')
+
+
+main()
